3258fdae6970d82a6a18e92be14b559fd5ba9735
Deepak Thakur <deepak2006ez@gmail.com>
2026-07-30 19:46:13 +0530

    Time: 0 ms (100%), Space: 42 MB (30.7%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -1,43 +1,22 @@
+//refer notes
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
-        int targetSumWindow=0;
-        int n=nums.size();
-        int ans=n;
-        for(int i=0;i<n;i++){
-            targetSumWindow+=nums[i];
-            if(targetSumWindow>=target){
-                ans=min(ans,i+1);
-                break;
-            }
-            if(i==(n-1)){
-                return 0;
-            }
-        }
         int left=0;
-        int right=ans-1;
-        if(right==n-1){
-            while(targetSumWindow>=target ){
-                targetSumWindow=targetSumWindow-nums[left];
-                if(targetSumWindow>=target){
-
-                    ans=min(ans,(right)-left);
-                }
+        int sum=0;
+        int n=nums.size();
+        int ans=INT_MAX;
+        for(int right=0;right<n;right++){
+            sum+=nums[right];
+            
+            while(sum>=target){
+                ans=min(ans,right-left+1);
+                sum-=nums[left];
                 left++;
             }
+           
         }
-        while(right<n-1){
-            targetSumWindow=targetSumWindow-nums[left]+nums[right+1];
-            while(targetSumWindow>=target){
-                left++;
-                targetSumWindow=targetSumWindow-nums[left];
-                ans=min(ans,(right+1)-left+1);
-            }
-            left++;
-            right++;
-
-        }
-        return ans;
+        return (ans==INT_MAX)? 0:ans;
         
     }
 };
