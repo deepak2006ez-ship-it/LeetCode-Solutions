629c7b32485f206ea7af594391635eca0f4f42dc
Deepak Thakur <deepak2006ez@gmail.com>
2026-09-11 22:29:40 +0530

    Time: 64 ms (39.02%), Space: 151.3 MB (37.94%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,34 +1,51 @@
 class MinStack {
 public:
-    stack<pair<int,int>>s;
+    stack<long long>s;
+    long long int minVal;
     MinStack() {
         
     }
     
     void push(int value) {
         if(s.empty()){
-            s.push({value,value});
-
+            s.push(value);
+            minVal=value;
+        }
+        else if(value>=minVal){
+            s.push(value);
         }else{
-            int minVal=min(value,s.top().second);
-            s.push({value,minVal});
-
+            long long val=2LL*value-minVal;//these we are doing so that when popping val from stack we can keep track of prev min value (minVal=2*value-val) val here while popping value will be curr min minVa
+            s.push(val);
+            minVal=value;
+            //val`=2(value)-minVal(these help to store both curr value and prev min,val` will be stored in stack(here val`==val))
         }
         
     }
     
     void pop() {
-        s.pop();
+        if(s.top()>=minVal){
+            s.pop();
+        }else{
+            minVal=2*minVal-s.top();
+            s.pop();
+            
+        }
         
     }
     
     int top() {
-        return s.top().first;
+        if(s.top()>=minVal){
+            return s.top();
+        }else{
+            return minVal;
+            
+        }
+
         
     }
     
     int getMin() {
-        return s.top().second;
+        return minVal;
     }
 };
 
