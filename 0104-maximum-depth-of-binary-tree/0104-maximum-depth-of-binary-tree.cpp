2f018ceae575325d630fe49053254952721bf95f
Deepak Thakur <deepak2006ez@gmail.com>
2026-09-19 10:34:07 +0530

    Time: 0 ms (100%), Space: 22.1 MB (87.33%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -1,40 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    int height=0;
-   
-    int maxDepth(TreeNode* root) {
-        queue<TreeNode*>q;
-        int height=0;
+    int heights=0;
+    void depthSearch(TreeNode* root,int height){
         if(root==NULL){
-            return height;
+            return;
         }
-        q.push(root);
-        q.push(NULL);
-        
-        while(!q.empty()){
-            TreeNode*curr=q.front();
-            q.pop();
-            if(curr==NULL){
-                if(!q.empty()){
-                    height++;
-                    q.push(NULL);
-                    continue;
+        heights=max(height,heights);
 
-                }else{
-                    break;
-
-                }
-            }
-            if(curr->left!=NULL){
-                q.push(curr->left);
-            }
-            if(curr->right!=NULL){
-                q.push(curr->right);
-            }
-
-
+        depthSearch(root->left,height+1);
+        depthSearch(root->right,height+1);
+    
+    }
+    int maxDepth(TreeNode* root) {
+        if(root==NULL){
+            return 0;
         }
-        return height+1;
-        
+        depthSearch(root,0);
+        return heights+1;    
     }
 };
