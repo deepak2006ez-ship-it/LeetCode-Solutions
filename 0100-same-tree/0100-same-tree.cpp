17fe087a7a65d122ae2391feff10b552a00648e2
Deepak Thakur <deepak2006ez@gmail.com>
2026-09-20 18:12:50 +0530

    Time: 0 ms (100%), Space: 12.8 MB (77.88%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -6,25 +6,18 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if(p==NULL && q==NULL){
-            return true;
+        if(p==NULL || q==NULL){
+            return p==q;
         }
-
-        if (p != NULL && q != NULL) {
-
-            if (p->val != q->val) {
-                return false;
-            }
-        }else{
-            return false;
-        }
-        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+        bool isLeftSame=isSameTree(p->left,q->left);
+        bool isRightSame=isSameTree(p->right,q->right);
+        return isLeftSame&&isRightSame&&p->val==q->val;
+        
     }
 };
