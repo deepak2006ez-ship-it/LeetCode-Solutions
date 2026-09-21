0a4a429246a4be0c96286ceb6954e50fac02e3a2
Deepak Thakur <deepak2006ez@gmail.com>
2026-09-21 20:10:09 +0530

    Time: 22 ms (61.25%), Space: 139.2 MB (73.49%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        deque<int>dq;
-        vector<int>ans;
-        for(int i=0;i<k;i++){
-            while(!dq.empty() && nums[dq.back()]<=nums[i]){
+        deque<int> dq;
+        vector<int> ans;
+        for (int i = 0; i < k; i++) {
+            while (!dq.empty() && nums[dq.back()] <= nums[i]) {
+
                 dq.pop_back();
             }
             dq.push_back(i);
         }
-        int n=nums.size();
-        for(int i=k;i<n;i++){
+        for (int i = k; i < nums.size(); i++) {
             ans.push_back(nums[dq.front()]);
-            while(!dq.empty() && dq.front()<=(i-k)){
-                dq.pop_front();
 
+            while (!dq.empty() && dq.front()<=(i - k)) {
+                dq.pop_front();
             }
-            while(!dq.empty() && nums[dq.back()]<=nums[i]){
+            while (!dq.empty() && nums[dq.back()] <= nums[i]) {
+
                 dq.pop_back();
             }
             dq.push_back(i);
